473c06d7407c563f3f72dabeb027271577a8fb0a
Oscar Kamonya <oscarkamonya@gmail.com>
2022-07-25 15:29:48 +0300

    poertfolio

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './footer.css'
+import {IoLogoLinkedin} from 'react-icons/io'
+import {IoLogoGithub} from 'react-icons/io'
+
+function Footer() {
+  return (
+    <footer>
+      <a href="#" className='footer__logo'>OSCAR</a>
+
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience"></a>Experience</li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonial">Testimonials</a></li>
+        <li><a href="#contact">Contacts</a></li>
+      </ul>
+
+      <div className="footer__social">
+      <a href="https://www.linkedin.com/in/oscar-kamonya-a2607a124/" target="_blank"><IoLogoLinkedin/></a>
+        <a href="https://github.com/Okamonya" target="_blank"><IoLogoGithub/></a>
+          <a href="https://likedin.com"></a>
+      </div>
+      <div className="footer__copyright">
+        <small>&copy; OSCAR Web Developer. All rights reserved.</small>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './header.css'
+import CTA from './CTA'
+import Profile from '../../assets/me.png'
+import Social from './Social'
+
+function Header() {
+  return (
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Oscar Kamonya</h1>
+        <h5 className="text-light">Java Web Developer</h5>
+        <CTA/>
+        <Social/>
+
+        <div className="profile">
+          <img src={Profile} alt="" />
+        </div>
+
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
+
+        
+      </div>
+    </header>
+  )
+}
+
+export default Header
